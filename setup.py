83f9cdd9c358fa9f7760b48aed19667c2fe5a4c4
Leopold Talirz <leopold.talirz@gmail.com>
2021-11-09 17:36:04 +0100

    chore: update dependencies

--- setup.py
+++ setup.py
@@ -4,38 +4,39 @@
 from setuptools import setup, find_packages
 
 if __name__ == '__main__':
-    setup(
-        name='nist-isotherm-digitizer',
-        packages=find_packages(),
-        long_description=open('README.md').read(),
-        long_description_content_type='text/markdown',
-        version='0.1.0',
-        description='Isotherm digitizer for the NIST Adsorption Database.',
-        author='Leopold Talirz',
-        author_email='leopold.talirz@gmail.com',
-        #url='to/be/decided',
-        install_requires=[
-            'panel~=0.11',
-            'bokeh~=2.3.0',
-            'traitlets~=4.3.3',
-            'requests~=2.24.0',
-            'requests_cache~=0.5.2',
-            'pandas~=1.0.5',
-            'pydenticon~=0.3.1',
-            #crossrefapi
-            #python-dateutil
-            #ase
-            #manage-crystal
-        ],
-        extras_require={
-            'pre-commit': [
-                'pre-commit~=2.2',
-                'pylint~=2.6.0',
+    with open('README.md', encoding='utf8') as handle:
+        setup(
+            name='nist-isotherm-digitizer',
+            packages=find_packages(),
+            long_description=handle.read(),
+            long_description_content_type='text/markdown',
+            version='0.1.0',
+            description='Isotherm digitizer for the NIST Adsorption Database.',
+            author='Leopold Talirz',
+            author_email='leopold.talirz@gmail.com',
+            #url='to/be/decided',
+            install_requires=[
+                'panel~=0.12.4',
+                'bokeh~=2.4.1',
+                'traitlets~=5.1.1',
+                'requests~=2.26.0',
+                'requests_cache~=0.8.1',
+                'pandas~=1.3.4',
+                'pydenticon~=0.3.1',
+                #crossrefapi
+                #python-dateutil
+                #ase
+                #manage-crystal
             ],
-            'tests': [
-                'pytest~=5.4',
-                'pytest-cov~=2.7',
-                'coverage<5.0',
-            ],
-        },
-    )
+            extras_require={
+                'pre-commit': [
+                    'pre-commit~=2.15',
+                    'pylint~=2.11.1',
+                ],
+                'tests': [
+                    'pytest~=5.4',
+                    'pytest-cov~=2.7',
+                    'coverage<5.0',
+                ],
+            },
+        )
